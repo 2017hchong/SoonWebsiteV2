b29e5c9ceb5c24bd2852344eae0121f0d7cb1d33
hayunchong <hayun99@gmail.com>
2020-08-28 10:05:49 -0400

    added components to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,13 @@ import {Link} from 'react-router-dom';
 import Arrow from "./Arrow";
 
 import "./styles/Home.css";
+
+import Container from 'react-bootstrap/Container';
 import Instagram from "./Instagram";
 import MainEventCard from "./MainEventCard";
 import PastEventCard from "./PastEventCard";
 import PrayerRequestsForm from "./PrayerRequestsForm";
+import InterestForm from "./InterestForm";
 
 
 // Example function for passing into arrow
@@ -17,9 +20,56 @@ function handleClick() {
 
 const Home = () => (
     <div>
+        <Container>
+            <div className={"maxBodyWidth"}>
+                <h2 className={"subheader whiteSpace"}>Who We Are</h2>
+                <div className={"body centeredText"}>
+                    Columbia SOON Movement is a Christian fellowship at Columbia University and an interdenominational ministry, previously known as KCCC. As part of SOON Movement Global (SMG),
+                    we are committed to spreading the Gospel of Jesus Christ to eveery campus and every nation.
+                    <br/><br/>
+                    <Link to={"/aboutus"} className={"linkClass"}> About Us </Link>
+                </div>
 
-        HOME!
-        <div className={"temp"}>
+                <h2 className={"subheader whiteSpace"}> Events </h2>
+                <MainEventCard
+                    img={"https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg"}
+                    title={"Large Group"}
+                    description={"Join us for our first official large group of the semester! This will be a great time to reconnect with fellow SOON members as well as welcome our newcomers!"}
+                    date={"September 10, 2020"}
+                    time={"8:00 PM EST"}
+                    location={"www.google.com"}
+                    isLink={true}
+                />
+
+                <h2 className={"subheader whiteSpace"}>Connect on Facebook</h2>
+                <div className={"body centeredText"}>
+                    We mainly communicate through our Facebook page, so please make sure to join our group for updates on upcoming events and other imporant announcements!
+                    <br/><br/>
+                    <a href={"https://www.facebook.com/groups/columbiakccc/"} className={"linkClass"}> See our Facebook Page </a>
+                </div>
+
+                <h2 className={"subheader whiteSpace"}>Instagram</h2>
+            </div>
+            <div id={"instaRow"}>
+                <Instagram
+                    instaLink={"https://www.instagram.com/p/B9jz-6FBOP-/?utm_source=ig_web_copy_link"}
+                />
+                <Instagram
+                    instaLink={"https://www.instagram.com/p/CEXq-7Sh2Zu/"}
+                />
+                <Instagram
+                    instaLink={"https://www.instagram.com/p/CEXqaM2BQOH/"}
+                />
+            </div>
+            <div className={"maxBodyWidth"}>
+                <div className={"whiteSpace"}>
+                    <InterestForm/>
+                </div>
+            </div>
+        </Container>
+
+        {/* ########### EXAMPLE COMPONENTS BELOW!!! #################*/}
+        <div className={"temp whiteSpace"}>
             <Arrow
                 isRight={true}
                 clickFunction={()=>handleClick()}
@@ -32,15 +82,6 @@ const Home = () => (
                 instaLink={"https://www.instagram.com/p/B9jz-6FBOP-/?utm_source=ig_web_copy_link"}
             />
         </div>
-        <MainEventCard
-            img={"https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg"}
-            title={"Large Group"}
-            description={"Join us for our first official large group of the semester! This will be a great time to reconnect with fellow SOON members as well as welcome our newcomers!"}
-            date={"September 10, 2020"}
-            time={"8:00 PM EST"}
-            location={"www.google.com"}
-            isLink={true}
-        />
         <PastEventCard
             img={"https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg"}
             title={"Large Group"}
@@ -50,7 +91,6 @@ const Home = () => (
 
         <Link className={"linkClass"}>Sample Link!</Link> <br/>
         <Link to={"/events"} className={"linkClass"}>Go to Events Page</Link>
-
     </div>
 );
 
