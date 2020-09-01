1e62e6a8fb18d1a04664a3739a828e21edb146a2
hayunchong <hayun99@gmail.com>
2020-09-01 10:05:21 -0400

    fixed announcements component

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -2,26 +2,41 @@
 import { Link } from 'react-router-dom';
 import "./styles/Announcements.css";
 
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
 class Announcements extends React.Component {
     render() {
+        const details = this.props.details.map((value, index) => (
+                validURL(value) ?
+                    <u> <a href={value} className={"details"}> {value} </a></u>
+                :
+                    <div> {value} <br/><br/></div>
+
+            ));
+
+        const annoucementsLink =
+            this.props.hasLink ?
+            <p id="announcementsLink">
+                <Link to={this.props.linkLocation} className={"linkClass"}>{this.props.linkName}</Link>
+            </p> : null;
+
         return (
             <div className= "announcementsFrame">
                 <div className={"body title"}>
-                    WELCOME BACK!
+                    {this.props.title}
                 </div>
                 <div className={"details announcement"}>
-                    Come learn more about our community and meet both returning and new members at our ⭐️ SOON SOCIAL ⭐️  on September 5th!
-                    <br /><br />
-                    If you're a newcomer, enter our ⬇️ RAFFLE GIVEAWAY ⬇️
-                    <br /><br />
-                    <u>
-                        <a href={"http://www.tinyurl.com/soonraffle"} className="details"> http://www.tinyurl.com/soonraffle  </a>
-                    </u>
+                    {details}
                 </div>
                 <br /><br />
-                <p id="announcementsLink">
-                    <Link to={"/events"} className={"linkClass"}> go to events </Link>
-                </p>
+                {annoucementsLink}
             </div>
         );
     }
